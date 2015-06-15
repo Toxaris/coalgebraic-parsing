567ed02a7805e04762e0d37c61bb4848540e6f85
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2015-06-15 19:23:20 +0200

    Inline ReaderT.
    With Jona.

--- src-lib/Text/CoalgebraicParsing.hs
+++ src-lib/Text/CoalgebraicParsing.hs
@@ -4,110 +4,88 @@
 module Text.CoalgebraicParsing
   ( -- * Parsers
     Parser
+    -- ** Destruct parsers
   , results
   , consume
   , parse
+    -- ** Construct parsers
+  , kill
   , token
   , intersect
   , neg
   , minus
-    -- * Transition tables   
-  , step
-  , skip
-  , kill
-  , cond
   ) where
 
 import Prelude hiding (foldl)
 
 import Control.Applicative
-import Control.Monad.Trans.Reader
-
 import Data.Foldable
-
--- | A transition table that maps all inputs to the same
--- behavior.
-skip :: f a -> ReaderT t f a
-skip p = ReaderT (\t -> p)
-
--- | A transition table that behaves different depending on a
--- predicate on tokens.
-cond :: (t -> Bool) -> f a -> f a -> ReaderT t f a
-cond f p q = ReaderT (\t -> if f t then p else q)
 
 -- | A parser that produces results of type 'a' in a data
 -- structure of type 'f' when fed tokens of type 't'.
 data Parser t f a = Parser
   { -- | Return the results if we would stop parsing now.
     results :: f a
-    -- | The transition table of this parser.
-  , step :: ReaderT t (Parser t f) a
+    -- | Consume a token and return a new parser that reflects the
+    -- consumed token in its internal state.
+  , consume :: t -> Parser t f a
   }
 
 instance Functor f => Functor (Parser t f) where
   fmap f p = Parser
     { results = fmap f (results p)
-    , step = fmap f (step p)
+    , consume = \t -> fmap f (consume p t)
     }
 
 instance Alternative f => Applicative (Parser p f) where
   pure a = Parser
     { results = pure a
-    , step = empty
+    , consume = \t -> empty
     }
 
   p <*> q = Parser
     { results = results p <*> results q
-    , step = step p <*> skip q <|> skip (kill p) <*> step q
+    , consume = \t -> consume p t <*> const q t <|> const (kill p) t <*> consume q t
     }
 
 instance Alternative f => Alternative (Parser t f) where
   empty = Parser
     { results = empty
-    , step = empty
+    , consume = \t -> empty
     }
 
   p <|> q = Parser
     { results = results p <|> results q
-    , step = step p <|> step q
+    , consume = \t -> consume p t <|> consume q t
     }
 
 -- | Remove a parser's future behavior.
 kill :: Alternative f => Parser t f a -> Parser t f a
-kill p = p { step = empty }
+kill p = p { consume = \t -> empty }
 
 -- | Accept exactly the given token.
 token :: (Alternative f, Eq t) => t -> Parser t f t
 token t = Parser
   { results = empty
-  , step = cond (t ==) (pure t) empty
+  , consume = \t' -> if t == t' then pure t else empty
   }
-
--- | Consume a token and return a new parser that reflects the
--- consumed token in its internal state.
-consume :: Parser t f a -> t -> Parser t f a
-consume = runReaderT . step
 
 -- | Parse a list of tokens.
 parse :: Parser t f a -> [t] -> f a
 parse p ts = results (foldl consume p ts)
 
-liftTable2 :: (f a -> f b -> f c) -> ReaderT t f a -> ReaderT t f b -> ReaderT t f c
-liftTable2 f tbl1 tbl2 =
-  ReaderT (\t -> f (runReaderT tbl1 t) (runReaderT tbl2 t))
-
 -- | Accept words that are accepted by both parsers.
 intersect :: Applicative f => Parser t f a -> Parser t f b -> Parser t f (a, b)
 intersect p q = Parser
   { results = (,) <$> results p <*> results q
-  , step = liftTable2 intersect (step p) (step q)
+  , consume = \t -> intersect (consume p t) (consume q t)
   }
 
 -- | Accept words that are *not* accepted by the parser.
 neg :: (Alternative f, Foldable f) => Parser t f a -> Parser t f ()
 neg p = Parser
   { results = if null (toList (results p)) then pure () else empty
-  , step = mapReaderT neg (step p)
+  , consume = \t -> neg (consume p t)
   }
 
 -- | Accept words accepted by the first but not the second parser.
